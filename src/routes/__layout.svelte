2e1a9219e73b688e0c12d7ae7cdb3e01f1d1538d
Remi Marche <remimarche@gmail.com>
2021-09-24 05:33:25 +0000

    remove code not working

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,16 @@
-<script context="module" lang="ts">
-    import { goto } from "$app/navigation";
-    import { locale } from "$lib/i18n";
+<script lang="ts">
+    // import { goto } from "$app/navigation";
+    // import { locale } from "$lib/i18n";
     import { onDestroy } from "svelte";
-    import { getStores } from "$app/stores";
-    import { get } from "svelte/store";
+    // import { getStores } from "$app/stores";
 
-    const { page } = getStores();
+    // const { page } = getStores();
 
-    let lastLocale = get(locale);
-    // let unsubscribe = () => {};
-    const unsubscribe = locale.subscribe((value) => {
-        goto(get(page)?.path?.replace(`/${lastLocale}/`, value))
-    });
+    // let lastLocale = $locale;
+    let unsubscribe = () => {};
+    // const unsubscribe = locale.subscribe((value) => {
+    //     goto($page?.path?.replace(`/${lastLocale}/`, value))
+    // });
 
     onDestroy(unsubscribe);
 </script>
