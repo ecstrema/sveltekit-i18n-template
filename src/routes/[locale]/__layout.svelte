100c97b21c258ae15948d82eff950921205edad8
Remi Marche <remimarche@gmail.com>
2021-09-27 18:34:50 -0400

    fixed language. Now working fine

---
+++ src/routes/[locale]/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { getStores } from "$app/stores";
+    import { locale } from "$lib/i18n";
+    import translations from "../../generated/translations";
+
+    const { page } = getStores();
+
+    if (Object.prototype.hasOwnProperty.call(translations, $page?.params?.locale))
+        $locale = $page.params.locale as typeof $locale;
+</script>
+
+<slot/>
