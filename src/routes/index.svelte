d71b111de33712a333b0400760bd568084e583b9
Remi Marche <remimarche@gmail.com>
2021-10-01 21:46:38 +0000

    fix url

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+    import { locale } from "$lib/i18n";
+    import { get as getStore } from "svelte/store";
+
+    /**
+     * @type {import('@sveltejs/kit').Get}
+     */
+    export async function get() {
+        return {
+            headers: {
+                Location: "/" + getStore(locale),
+            },
+            status: 302,
+        }
+    }
+</script>
