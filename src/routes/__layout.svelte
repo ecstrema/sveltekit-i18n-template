125e25aca9f7dad074fb84ce90ae71dd5fd4dc90
Remi Marche <remimarche@gmail.com>
2021-10-01 22:49:30 +0000

    redirection fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import { base } from "$app/paths";
     import { getStores } from "$app/stores";
     import { locale } from "$lib/i18n";
-    import { onDestroy,onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
 
     let lastLocale = $locale;
 
     const { page } = getStores();
 
     function localizedPath(path: string, lastLocale: string, newLocale: string): string {
-        return "/" + newLocale + path.substring(lastLocale.length + 1);
+        return (base ? "/" + base : "") + "/" + newLocale + path.substring(lastLocale.length + 1);
     }
 
     let unsubscribe = () => {};
     // Run inside onMount to be only run client-side
     onMount(() => {
         unsubscribe = locale.subscribe((newLocale) => {
-            goto(localizedPath($page.path, lastLocale, newLocale));
+            goto(localizedPath($page.path, lastLocale, newLocale), { replaceState: true });
             lastLocale = newLocale;
         });
     });
