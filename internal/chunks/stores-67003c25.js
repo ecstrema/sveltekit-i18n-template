4b7db7465338226f6822204bceab440307e78177
Remi Marche <remimarche@gmail.com>
2021-10-01 22:07:32 +0000

    Updates

--- internal/chunks/stores-67003c25.js
+++
@@ -1,1 +0,0 @@
-import{J as e}from"./vendor-47aff0e7.js";const t=()=>{const s=e("__svelte__");return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},get preloading(){return console.error("stores.preloading is deprecated; use stores.navigating instead"),{subscribe:s.navigating.subscribe}},session:s.session}};export{t as g};
